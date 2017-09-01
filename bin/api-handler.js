f47f0ede0a0ab8078dcf6f6690403f607552e1b0
Fabricio Borgatto <fabricioborgatto@gmail.com>
2017-09-01 00:13:21 -0300

    new file:   bin/api-handler.js

---
+++ bin/api-handler.js
@@ -0,0 +1,27 @@
+'use strict';
+var request = require('request');
+var qs = require('querystring');
+
+exports.retrieveFromApi = function (characterName) {
+
+    var post_url = 'http://stapi.co/api/v1/rest/character/search';
+    var post_qs = { name: characterName };
+
+    var get_url = 'http://stapi.co/api/v1/rest/character';
+
+    request.post({ url: post_url, qs: post_qs }, (error, response, body) => {
+        var api_response = JSON.parse(body);
+        var uid = api_response.characters[0].uid;
+
+        var get_qs = { uid: uid };
+
+        request.get({ url: get_url, qs: get_qs }, (error, response, body) => {
+            var get_response = JSON.parse(body);
+            var species = get_response.character.characterSpecies[0].name;
+            console.log(species);
+        });
+    });
+
+};
+
+
