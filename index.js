fc375cf6107a05f1bd6eafc550d490d6149cf3d4
Fabricio Borgatto <fabricioborgatto@gmail.com>
2017-09-01 01:13:07 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ var api = require('./bin/api-handler');
 
 var name = process.argv.splice(2, process.argv.length -1).join(' ');
 
-console.log(name);
-
 app.getNameInHexa(name);
 api.retrieveFromApi(name);
